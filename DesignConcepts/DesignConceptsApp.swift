f25f3bc1859ca456edc48ea6b3ca1b417c3cf5c0
Nicholas Pompea <nick.pompea@gmail,com>
2022-10-14 15:32:50 -0600

    Initial Commit

---
+++ DesignConcepts/DesignConceptsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignConceptsApp.swift
+//  DesignConcepts
+//
+//  Created by Nicholas Pompea on 10/14/22.
+//
+
+import SwiftUI
+
+@main
+struct DesignConceptsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
